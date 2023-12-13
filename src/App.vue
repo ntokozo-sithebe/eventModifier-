8a03e4135f15da0d3dfa30eb6a3d0bddc0fb6eea
ntokozo-sithebe <ntoky28@gmail.com>
2023-12-13 12:03:53 +0200

    event modifier

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit.prevent="sendInfo">
+  <div>
+  <label> Enter a value</label>
+  <input type="text" placeholder="Enter your value"
+  v-model="currValue" @keyup.enter="popUpMessage(currValue)"  >
+  <!--  -->
+</div>
+<div>
+  <h2> List of entered values</h2>
+  <ul v-show="listOfValues.length">
+   <li v-for="data, index in listOfValues" :key="index">   
+    <p> {{ data }} @ position {{ index }}</p> 
+   </li>
+  </ul>
+</div>
+<button type="submit" @click.prevent="sendInfo">Display</button>
+</form>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      currValue: '',
+      listOfValues: []
+    }
+  },
+   components:{},
+   methods:{
+    sendInfo(){
+      if(this.currValue)
+      this.listOfValues.push(this.currValue)
+    },
+    popUpMessage(arg){
+      alert(arg)
+    }
   }
+
 }
 </script>
 
